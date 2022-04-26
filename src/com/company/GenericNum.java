668f4dad1d677dd16da1441bb49d611726824169
berdibekkurbanbaev <kurbanbaevberdibek@gmail.com>
2022-04-26 22:33:06 +0300

    Initial commit

---
+++ src/com/company/GenericNum.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class GenericNum {
+    private int numbers;
+
+    public int getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int numbers) {
+        this.numbers = numbers;
+    }
+
+    public GenericNum(int numbers) {
+        this.numbers = numbers;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<GenericNum> list = new ArrayList<>();
+        list.add(new GenericNum(1));
+        list.add(new GenericNum(4));
+        list.add(new GenericNum(9));
+        list.add(new GenericNum(7));
+        list.add(new GenericNum(4));
+        list.add(new GenericNum(10));
+    }
+    public static void method(List<Integer>list){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.size())
+
+        }
+
+    }
+}
